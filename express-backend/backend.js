118725af407c6e03c43560602aa5a2d342f57fa2
vilinh <vilinhlvu@gmail.com>
2023-09-25 18:20:19 -0700

    add filter by id

--- express-backend/backend.js
+++ express-backend/backend.js
@@ -50,9 +50,24 @@ app.get("/users", (req, res) => {
   }
 });
 
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"];
+  let result = findUserById(id);
+  if (result === undefined || result.length == 0)
+    res.status(404).send("Resource not found.");
+  else {
+    result = { users_list: result };
+    res.send(result);
+  }
+});
+
 const findUserByName = (name) => {
   return users["users_list"].filter((user) => user["name"] === name);
 };
+
+function findUserById(id) {
+    return users["users_list"].filter((user) => user['id'] === id)
+}
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
