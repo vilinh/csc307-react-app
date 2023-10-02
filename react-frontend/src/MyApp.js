65020fce0b7c2b3c2a27c3e6832d05f3ab89d7d7
vilinh <vilinhlvu@gmail.com>
2023-10-02 10:43:39 -0700

    submit button and POST call to add users

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -25,15 +25,28 @@ function MyApp() {
     });
   }, []);
 
+  async function makePostCall(person) {
+    try {
+      const response = await axios.post("http://localhost:8000/users", person);
+      return response;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+
+  function updateList(person) {
+    makePostCall(person).then(result => {
+      if (result && result.status === 200)
+        setCharacters([...characters, person]);
+    })
+  }
+
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
       return i !== index;
     });
     setCharacters(updated);
-  }
-
-  function updateList(person) {
-    setCharacters([...characters, person]);
   }
 
   return (
